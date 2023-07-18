3f335fe32f28f1db835b11ce8a351ef581950920
phaneendra <phani.manthena27@gmail.com>
2023-07-18 15:28:54 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,14 +1,13 @@
 def call() {
     // Define your shared library logic here, if needed
     // This method will be called when the shared library is used in a Jenkinsfile
-}
-
-pipeline {
-    agent any
-    stages {
-        stage('Compile/Build') {
-            steps {
-                echo 'Compiling'
+    pipeline {
+        agent any
+        stages {
+            stage('Compile/Build') {
+                steps {
+                    echo 'Compiling'
+                }
             }
         }
     }
