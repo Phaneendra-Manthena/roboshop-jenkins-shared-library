402736d2e0c59c4426d2cd3f57cfe13b85a7d9d5
phaneendra <phani.manthena27@gmail.com>
2023-07-18 15:27:16 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -11,15 +11,5 @@ pipeline {
                 echo 'Compiling'
             }
         }
-        stage('Unit Tests') {
-            steps {
-                echo 'Unit Tests'
-            }
-        }
-        stage('Quality Control') {
-            steps {
-                echo 'Quality Control'
-            }
-        }
     }
 }
