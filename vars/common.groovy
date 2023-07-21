988083db948fb156933a7657aea57bf4c7d6ebed
phaneendra <phani.manthena27@gmail.com>
2023-07-21 16:17:07 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -26,5 +26,5 @@ def dependencyCheck() {
 
 def email(email_notes){
     mail bcc: '', body: "Job Failed - ${JOB_BASE_NAME}\nJenkins URL - ${JOB_URL}", cc: '', from: 'mpvarma9997@gmail.com', replyTo: '', subject: "Jenkins Job Failed - ${JOB_BASE_NAME}", to: 'phani.manthena27@gmail.com'
-}
+
 }
