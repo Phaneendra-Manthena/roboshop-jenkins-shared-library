9767abfb279c732a1d300364107e85ed2372728a
phaneendra <phani.manthena27@gmail.com>
2023-07-21 14:08:54 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -8,7 +8,12 @@ def compile() {
 }
 def unittests() {
     if (app_lang == "nodejs") {
-        sh 'npm test || true'
+        try {
+            sh 'npm test || true'
+        } catch (Exception e) {
+            common.email("Unit tests failed")
+        }
+
     }
     if (app_lang == "java") {
         sh 'mvn test'
@@ -16,4 +21,8 @@ def unittests() {
     if (app_lang == "python") {
         sh 'python3 -m unittest'
     }
+}
+
+def email(email_notes){
+    sh 'echo ${email_notes}'
 }
