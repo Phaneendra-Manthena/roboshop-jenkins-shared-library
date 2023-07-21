c45840aa25ed8ab4b8cbf9c16d3817d7142af71c
phaneendra <phani.manthena27@gmail.com>
2023-07-21 14:57:07 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,39 +1,39 @@
 def call() {
-//    try {
-        pipeline {
-            agent {
-                label 'workstation'
+    // try {
+    pipeline {
+        agent {
+            label 'workstation'
+        }
+        stages {
+            stage('Compile/Build') {
+                steps {
+                    script {
+                        common.compile()
+                    }
+                }
             }
-            stages {
-                stage('Compile/Build') {
-                    steps {
-                        script {
-                            common.compile()
-                        }
+            stage('Unit Tests') {
+                steps {
+                    script {
+                        common.unittests()
                     }
                 }
-                stage('Unit Tests') {
-                    steps {
-                        script {
-                            common.unittests()
-                        }
+            }
+            stage('OWASP Dependency Check') {
+                steps {
+                    script {
+                        common.dependencyCheck()
                     }
                 }
-                stage('OWASP Dependency Check') {
-                    steps {
-                        script {
-                            common.dependencyCheck()
-                        }
-                    }
-                }
-                stage('Quality Control') {
-                    steps {
-                        echo 'Quality Control'
-                    }
+            }
+            stage('Quality Control') {
+                steps {
+                    echo 'Quality Control'
                 }
             }
         }
-//    } catch (Exception e) {
-//        common.email("Failed")
-//    }
+    }
+    // } catch (Exception e) {
+    //    common.email("Failed")
+    // }
 }
