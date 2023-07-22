b626273a531ffcb683c9b9ef2163ed1486658124
phaneendra <phani.manthena27@gmail.com>
2023-07-22 16:53:46 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -36,16 +36,19 @@ def call() {
                 }
                 stage('Quality Control') {
                     environment {
-                        // ... (rest of the environment configuration)
+//                        SONAR_PASS = sh(script: "aws secretsmanager get-secret-value --secret-id sonar.pass | jq -r '.SecretString'", returnStdout: true).trim()
+                        SONAR_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+
+                        SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+
                     }
                     steps {
                         wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-                            // ... (rest of the steps)
+//                            sh "sonar-scanner -Dsonar.host.url=http://34.124.155.157:9000 -Dsonar.login='${SONAR_USER}' -Dsonar.password='${SONAR_PASS}' -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
                             sh "echo Sonar Scan"
                         }
                     }
                 }
-            }
             // Move the 'Upload to Centralized Place' stage outside of the 'stages' block
             stage('Upload to Centralized Place') {
                 steps {
