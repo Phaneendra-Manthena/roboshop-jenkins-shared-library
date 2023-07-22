73a2075fc818c97b6f5377c270682a10e5181ffd
phaneendra <phani.manthena27@gmail.com>
2023-07-22 15:03:10 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -37,11 +37,19 @@ def call() {
                     }
                 }
                 stage('Quality Control') {
+                    environment {
+//                        SONAR_PASS = sh(script: "aws secretsmanager get-secret-value --secret-id sonar.pass | jq -r '.SecretString'", returnStdout: true).trim()
+                        SONAR_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+//
+                        SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                    }
                     steps {
-                        script{
-                            common.sonarQubecheck()
+                        wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+                            sonarScan(sonarUrl: 'http://34.124.155.157:9000', sonarUser: '${SONAR_USER}', sonarPass: '${SONAR_USER}', component: '${component}', sonarExtraOpts: '${SONAR_EXTRA_OPTS}')
+//                            sh "echo Sonar Scan"
                         }
                     }
+
                 }
                 stage('Cleaning WorkSpace') {
                     steps{
--- vars/common.groovy
+++ vars/common.groovy
@@ -26,18 +26,8 @@ def dependencyCheck() {
         echo 'Skipping Dependency checks'
     }
 }
-def sonarQubecheck(sonarQubecheck) {
-    if (sonarQubecheck == "true") {
-        // Retrieve SonarQube credentials from AWS SSM Parameter Store
-         SONAR_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-         SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-
-        // Wrap the password to mask it in the Jenkins build log
-        wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-            // Execute SonarQube analysis
-            sh "sonar-scanner -Dsonar.host.url=http://34.124.155.157:9000 -Dsonar.login='${SONAR_USER}' -Dsonar.password='${SONAR_PASS}' -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
-        }
-    }
+def sonarScan(String sonarUrl, String sonarUser, String sonarPass, String component, String sonarExtraOpts) {
+    sh "sonar-scanner -Dsonar.host.url=${sonarUrl} -Dsonar.login=${sonarUser} -Dsonar.password=${sonarPass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${sonarExtraOpts}"
 }
 
 def email(email_note){
