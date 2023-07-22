2b2da572d51c42ce7e5b1adc625891b07b276b9c
phaneendra <phani.manthena27@gmail.com>
2023-07-22 18:22:25 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,17 +1,19 @@
 def call() {
     if (!env.SONAR_EXTRA_OPTS) {
-        env.SONAR_EXTRA_OPTS = " "
-    }
-    if (!env.TAG_NAME) {
-        env.PUSH_CODE = "false"
-    } else {
-        env.PUSH_CODE = "true"
-    }
+            env.SONAR_EXTRA_OPTS = " "
+        }
+        if (!env.TAG_NAME) {
+            env.PUSH_CODE = "false"
+        } else {
+            env.PUSH_CODE = "true"
+        }
+
     try {
         pipeline {
             agent {
-                label 'workstation'
+               label 'workstation'
             }
+
             stages {
                 stage('Compile/Build') {
                     steps {
@@ -35,37 +37,34 @@ def call() {
                     }
                 }
                 stage('Quality Control') {
-                    environment {
-//                        SONAR_PASS = sh(script: "aws secretsmanager get-secret-value --secret-id sonar.pass | jq -r '.SecretString'", returnStdout: true).trim()
-                        SONAR_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-
-                        SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-
-                    }
                     steps {
+                        environment {
+                           SONAR_PASS = sh(script: "aws secretsmanager get-secret-value --secret-id sonar.pass | jq -r '.SecretString'", returnStdout: true).trim()
+                                                   SONAR_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
 
+                                                   SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                        }
+                        script {
                             wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-//                            sh "sonar-scanner -Dsonar.host.url=http://34.124.155.157:9000 -Dsonar.login='${SONAR_USER}' -Dsonar.password='${SONAR_PASS}' -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
-                                sh "echo Sonar Scan"
-                            }
-                    }
-
-                    stage('Upload to Centralized Place') {
-                        steps {
-                            script {
-
-                                if (env.PUSH_CODE == "true") {
-                                    echo 'Uploading to Centralized Place'
-                                }
+                               sh "sonar-scanner -Dsonar.host.url=http://34.124.155.157:9000 -Dsonar.login='${SONAR_USER}' -Dsonar.password='${SONAR_PASS}' -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
+                                                               sh "echo Sonar Scan"
                             }
                         }
                     }
-
-                    stage('Cleaning WorkSpace') {
-                        steps {
-                            script {
-                                cleanWs()
+                }
+                stage('Upload to Centralized Place') {
+                    steps {
+                        script {
+                            if (env.PUSH_CODE == "true") {
+                                echo 'Uploading to Centralized Place'
                             }
+                        }
+                    }
+                }
+                stage('Cleaning WorkSpace') {
+                    steps {
+                        script {
+                            cleanWs()
                         }
                     }
                 }
