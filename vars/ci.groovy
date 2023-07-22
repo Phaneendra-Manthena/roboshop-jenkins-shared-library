b599a151faae327cef7b2b29a5b992d273346020
phaneendra <phani.manthena27@gmail.com>
2023-07-22 14:43:19 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -39,7 +39,7 @@ def call() {
                 stage('Quality Control') {
                     steps {
                         script{
-                            common.sonarQubecheck(true)
+                            common.sonarQubecheck()
                         }
                     }
                 }
