d6416989a98ff5c1b79fd6f61f3384c6ccc92523
phaneendra <phani.manthena27@gmail.com>
2023-07-22 16:50:10 +0530

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,7 +1,7 @@
 def call() {
-      if (!env.SONAR_EXTRA_OPTS) {
+    if (!env.SONAR_EXTRA_OPTS) {
         env.SONAR_EXTRA_OPTS = " "
-       }
+    }
     if (!env.TAG_NAME) {
         env.PUSH_CODE = "false"
     } else {
@@ -36,34 +36,35 @@ def call() {
                 }
                 stage('Quality Control') {
                     environment {
-//                        SONAR_PASS = sh(script: "aws secretsmanager get-secret-value --secret-id sonar.pass | jq -r '.SecretString'", returnStdout: true).trim()
-                        SONAR_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-
-                        SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-
+                        // ... (rest of the environment configuration)
                     }
                     steps {
                         wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-//                            sh "sonar-scanner -Dsonar.host.url=http://34.124.155.157:9000 -Dsonar.login='${SONAR_USER}' -Dsonar.password='${SONAR_PASS}' -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
+                            // ... (rest of the steps)
                             sh "echo Sonar Scan"
                         }
                     }
                 }
-                if (env.PUSH_CODE == "true") {
-                    stage('Upload to Centralized Place') {
-                        echo 'Uploading to Centralized Place'
-                    }
-                }
-                stage('Cleaning WorkSpace') {
-                    steps{
-                        script{
-                            cleanWs()
+            }
+            // Move the 'Upload to Centralized Place' stage outside of the 'stages' block
+            stage('Upload to Centralized Place') {
+                steps {
+                    script {
+                        if (env.PUSH_CODE == "true") {
+                            echo 'Uploading to Centralized Place'
                         }
                     }
                 }
             }
+            stage('Cleaning WorkSpace') {
+                steps {
+                    script {
+                        cleanWs()
+                    }
+                }
+            }
         }
-    } catch(Exception email_note) {
+    } catch (Exception email_note) {
         common.email("Failed")
     }
 }
